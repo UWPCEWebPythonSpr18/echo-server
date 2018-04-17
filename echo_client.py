71123c4e66c9a39beba173b4a583e81cb0f1f0cf
Dan K <kuchandw@gmail.com>
2018-04-16 18:22:58 -0700

    cleaning
    cleaned up client a tad

--- echo_client.py
+++ echo_client.py
@@ -9,14 +9,7 @@ def client(msg, log_buffer=sys.stderr):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
     sock.connect(server_address)
-    # TODO: connect your socket to the server here.
-
-    # you can use this variable to accumulate the entire message received back
-    # from the server
     received_message = ''
-
-    # this try/finally block exists purely to allow us to close the socket
-    # when we are finished with it
     try:
         print('sending "{0}"'.format(msg), file=log_buffer)
         sock.sendall(msg.encode('utf8'))
@@ -28,15 +21,10 @@ def client(msg, log_buffer=sys.stderr):
                 print("Reached EOM")
                 break
     finally:
-        # TODO: after you break out of the loop receiving echoed chunks from
-        #       the server you will want to close your client socket.
         sock.close()
         print('Closing Socket', file=log_buffer)
         print("Message Recieved: {}".format(received_message))
-        # TODO: when all is said and done, you should return the entire reply
-        # you received from the server as the return value of this function.
     return received_message
-
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
